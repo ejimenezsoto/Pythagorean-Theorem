214cae1ef2710b79c7a2a115430c371d395daf1b
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-02-13 16:02:44 -0800

    Finished Pyth program

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main (String[] args) {
+
+        Pythagorean pythagorean = new Pythagorean();
+
+        double output = pythagorean.calculateHypotenuse(3, 3);
+
+        System.out.println(output);
+    }
+}
